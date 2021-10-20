ff0c4937957651a50882b8847c0a0b802d781441
kamocyc <kamocyc@users.noreply.github.com>
2021-10-20 15:01:24 +0900

    delete unused files

--- benchmark/orig/murase_closure.ml
+++
@@ -1,8 +0,0 @@
-let const x () = x
-let rec finish() = event "A"; finish()
-let rec f g = let n = g() in if n>0 then f (const (n-1)) else finish()
-let main = let n = Random.int(0) in f (const n)
-
-(*{SPEC}
-  fairness: (A, Never)
-{SPEC}*)
--- benchmark/orig/murase_intro.ml
+++
@@ -1,8 +0,0 @@
-let rec f x =
-  if x < 0 then ()
-  else if x = 0 then (event "A")
-  else (f 0; f 1)
-
-(*{SPEC}
-  fairness: (A, Never)
-{SPEC}*)
