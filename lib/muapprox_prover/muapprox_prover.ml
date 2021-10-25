2c0a41523b88b3589ab93d4f51220ed9df4d525b
kamocyc <kamocyc@users.noreply.github.com>
2021-10-25 12:54:07 +0900

    improve sub-process management

--- lib/muapprox_prover/muapprox_prover.ml
+++ lib/muapprox_prover/muapprox_prover.ml
@@ -225,8 +225,8 @@ module SolverCommon = struct
     message_string ~header:"Result" @@ Status.string_of res ^ " / " ^ log_message;
     res
   
-  let run_command_with_timeout ?no_quote timeout command mode =
-    unix_system ?no_quote timeout command mode
+  let run_command_with_timeout timeout command mode =
+    unix_system timeout command mode
     
 end
 
@@ -459,11 +459,12 @@ module SuzukiSolver : BackendSolver = struct
     let path = save_hes_to_file hes debug_context in
     let debug_context = Option.map (fun d -> { d with temp_file = path }) debug_context in
     let command = solver_command path solve_options in
-    run_command_with_timeout ~no_quote:true solve_options.timeout command (Option.map (fun c -> c.mode) debug_context)
+    failwith "not implemented"
+    (* run_command_with_timeout ~no_quote:true solve_options.timeout command (Option.map (fun c -> c.mode) debug_context)
     >>| (fun (status_code, elapsed, stdout, stderr) ->
         try
           parse_results (status_code, stdout, stderr) debug_context elapsed
-          with _ -> Status.Unknown)
+          with _ -> Status.Unknown) *)
 end
 
 let rec is_first_order_function_type (ty : Hflmc2_syntax.Type.simple_ty) =
--- lib/muapprox_prover/unix_command.ml
+++ lib/muapprox_prover/unix_command.ml
@@ -1,4 +1,5 @@
 type process_status = Unix.process_status
+module Unix_old = Unix
 
 open Async
 
@@ -6,6 +7,7 @@ let log_src = Logs.Src.create "Unix_command"
 module Log = (val Logs.src_log @@ log_src)
 
 let log_string = Manipulate.Hflz_util.log_string Log.info
+let warn_string = Manipulate.Hflz_util.log_string Log.warn
 
 (* This mutable data structure is accessed by multiple threads.
    However, this is safe because Async uses cooperative multithreading (not preemptive threads)
@@ -18,6 +20,27 @@ let append_to_hashtbl_of_list tbl k e =
   | None -> Hashtbl.add tbl k [e]
   | Some l -> Hashtbl.replace tbl k (e::l)
 
+let pp_process_result fmt stat =
+  let show_process_status : process_status -> string = function
+    | WEXITED code -> "WEXITED(" ^ (string_of_int code) ^ ")"
+    | WSIGNALED code -> "WSIGNALED(" ^ (string_of_int code) ^ ")"
+    | WSTOPPED code -> "WSTOPPED(" ^ (string_of_int code) ^ ")" in
+  Format.pp_print_string fmt @@ "Process result:\n";
+  (* Format.pp_print_string fmt @@ "out: " ^ out ^ "\n"; *)
+  Format.pp_print_string fmt @@ "status: " ^ (show_process_status stat) ^ "\n"
+
+let show_code (code : (unit, [ `Exit_non_zero of int | `Signal of Hflmc2_util.Core.Signal.t ]) result) =
+  match code with
+  | Ok () -> "Ok"
+  | Error code -> begin
+    match code with
+    | `Exit_non_zero code ->
+      "`Exit_non_zero (" ^ string_of_int code ^ ")"
+    | `Signal signal -> begin
+      "`Signal (" ^ Signal.to_string signal ^ ")"
+    end
+  end
+
 let kill_processes mode =
   match mode with
   | None -> Deferred.return ()
@@ -26,94 +49,87 @@ let kill_processes mode =
     match Hashtbl.find_opt pids mode with
     | None -> Deferred.return ()
     | Some pid_filenames -> begin
-      (* print_endline @@ "mode=" ^ mode ^ ", pid_filenames=" ^ (String.concat ", " pid_filenames);
-      Hashtbl.remove pids mode; *)
+      Hashtbl.remove pids mode;
       let comma_sep_pid_filenames = List.map String.trim pid_filenames |> (String.concat ",") in
-      (* https://stackoverflow.com/a/3211182/12976091 *)
-      (* https://stackoverflow.com/a/13481601/12976091 *)
-      let killptree_command = {|
-      killtree() {
-        local _pid=$1
-        local _sig=${2:-TERM}
-        kill -stop ${_pid} # needed to stop quickly forking parent from producing child between child killing and parent killing
-        for _child in $(ps -o pid --no-headers --ppid ${_pid}); do
-            # echo pid: ${_pid} " V " ${_child}
-            killtree ${_child} ${_sig}
-        done
-        kill -${_sig} ${_pid}
-        kill -cont ${_pid}  # stopped processes are not SIGTERMed!!
-        # wait ${_pid} 2>/dev/null || true  # for suppress "Terminated" message, but it takes time(?)
-      }
+      let command = {|
       for pid in `echo |} ^ comma_sep_pid_filenames ^ {| | sed "s/,/ /g"`
       do
-        killtree `cat $pid`
+        kill -`cat $pid` 2> /dev/null
       done
       |} in
-      let command = "bash -c '" ^ killptree_command ^ "' 2> /dev/null " in
-      (* let process_names = ["hflmc2"; "main.exe"; "z3"; "hoice"] in
-      let kill_command = String.concat "; " (List.map (fun n -> "pkill " ^ n) process_names) in *)
-      (* save_string_to_file "/tmp/aaaaa.txt" command; *)
-      (* print_endline @@ "kill command=" ^ command; *)
-      (* failwith "a"; *)
-      Unix.system command
-    end >>| (fun _ -> Hashtbl.remove pids mode; ())
+      Unix.system @@ command
+      >>| (fun code ->
+        match code with
+        | Ok () -> begin
+          ()
+        end
+        | Error code -> begin
+          warn_string @@ "Error status (kill_processes, " ^ show_code (Error code) ^ ")"
+      end
+      )
+    end
   end
 
-  let pp_process_result fmt stat =
-    let show_process_status : process_status -> string = function
-      | WEXITED code -> "WEXITED(" ^ (string_of_int code) ^ ")"
-      | WSIGNALED code -> "WSIGNALED(" ^ (string_of_int code) ^ ")"
-      | WSTOPPED code -> "WSTOPPED(" ^ (string_of_int code) ^ ")" in
-    Format.pp_print_string fmt @@ "Process result:\n";
-    (* Format.pp_print_string fmt @@ "out: " ^ out ^ "\n"; *)
-    Format.pp_print_string fmt @@ "status: " ^ (show_process_status stat) ^ "\n"
+let () =
+  Signal.handle Signal.terminating ~f:(fun signal ->
+    let pid_filenames = Hashtbl.fold (fun _ v acc -> List.concat [v; acc]) pids [] in
+    
+    let comma_sep_pid_filenames = List.map String.trim pid_filenames |> (String.concat ",") in
+    let command = {|
+    for pid in `echo |} ^ comma_sep_pid_filenames ^ {| | sed "s/,/ /g"`
+    do
+      echo KILL2: "-|} ^ Signal.to_string signal ^ {| `cat $pid`"
+      bash -c "kill -|} ^ Signal.to_string signal ^ {| `cat $pid`" # 2> /dev/null
+    done
+    |} in
+    let code = Unix_old.system command in
+    (match code with
+    | WEXITED 0 -> ()
+    | _ -> begin
+      warn_string "Error when killing processes";
+      pp_process_result Format.std_formatter code
+    end);
+    ignore @@ Signal.default_sys_behavior signal;
+    shutdown 0
+  )
   
-  let show_code (code : (unit, [ `Exit_non_zero of int | `Signal of Hflmc2_util.Core.Signal.t ]) result) =
-    match code with
-    | Ok () -> "Ok"
-    | Error code -> begin
-      match code with
-      | `Exit_non_zero code ->
-        "`Exit_non_zero (" ^ string_of_int code ^ ")"
-      | `Signal signal -> begin
-        "`Signal (" ^ Signal.to_string signal ^ ")"
-      end
-    end
+let unix_system timeout commands mode =
+  let open Core in
+  let module Proc = Async_unix.Process in
+  let command, arguments =
+    let commands =
+      if true then Array.to_list commands
+      else Array.to_list commands |> List.map ~f:(fun c -> "\"" ^ c ^ "\"") 
+      in
+    "timeout", [string_of_int timeout] @ commands in
   
-let unix_system ?(no_quote=false) timeout commands mode =
-  (* quote *)
-  (* 環境変数をセットするためにquoteしない *)
-  let commands =
-    if no_quote then Array.to_list commands
-    else Array.to_list commands |> List.map (fun c -> "\"" ^ c ^ "\"") 
-    in
+  log_string @@ "Run command: " ^ command ^ " " ^ (String.concat ~sep:" " arguments);
+  
   let r = Random.int 0x10000000 in
-  let stdout_name = Printf.sprintf "/tmp/%d_stdout.tmp" r in
-  let stderr_name = Printf.sprintf "/tmp/%d_stderr.tmp" r in
   let pid_name = Printf.sprintf "/tmp/%d_pid.tmp" r in
-  let commands = commands @ [">"; stdout_name; "2>"; stderr_name] in
-  
-  log_string @@ "Run command: " ^ (String.concat " " commands);
-  
-  let command = String.concat " " commands in
-  let command = command ^ " &\nbpid=$!\necho $bpid > " ^ pid_name ^ "\nwait $bpid" in
-  
   (match mode with
   | Some mode -> append_to_hashtbl_of_list pids mode pid_name
   | None -> ());
   
   let start_time = Stdlib.Sys.time () in
   
-  let deferred_main = Unix.system command in
-  let deferred_timeout = Clock.after (Core.Time.Span.of_sec (float_of_int timeout)) >>| (fun () -> Error (`Exit_non_zero 124)) in
-  
-  Deferred.any [deferred_main; deferred_timeout]
-  >>= (fun code ->
-    let elapsed = Stdlib.Sys.time () -. start_time in
-    (* print_endline @@ "ALL COMMAND: \"" ^ command ^ "\"\n" ^ "CODE: " ^ show_code code; *)
-    Reader.file_contents stdout_name >>= (fun stdout ->
-      Reader.file_contents stderr_name >>| (fun stderr ->
-        code, elapsed, stdout, stderr
+  Proc.create ~prog:command ~args:arguments ()
+  >>= (fun process_opt ->
+    match process_opt with
+    | Ok process -> begin
+      let pid = Proc.pid process in
+      let pid_i = Pid.to_int pid in
+      Writer.save pid_name ~contents:(string_of_int pid_i ^ "\n")
+      >>= (fun () ->
+        Proc.collect_output_and_wait process
+        >>| (fun output ->
+          let elapsed = Stdlib.Sys.time () -. start_time in
+          let {Proc.Output.stdout; stderr; exit_status} = output in
+          exit_status, elapsed, stdout, stderr
+        )
       )
-    )
+    end
+    | Error result -> begin
+      failwith @@ "Command error (1): " ^ Error.to_string_hum result 
+    end
   )
