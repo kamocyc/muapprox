243cda6f4c622ea2ac58db9167a2170fe1285b9f
kamocyc <kamocyc@users.noreply.github.com>
2021-10-27 15:51:19 +0900

    fix script

--- benchmark/run_muapprox_katsura.sh
+++ benchmark/run_muapprox_katsura.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-$katsura_solver_path \
+SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)"
+"$(dirname -- "$SCRIPT_DIR")"/_build/default/bin/muapprox_main.exe \
   --solver katsura --verbose "${@:2}" \
   "$1" > /tmp/stdout_1.txt 2> /tmp/stderr_1.txt
 # --replacer="$(basename "$1" .in)" 
